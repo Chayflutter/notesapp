2a2e0e1924f96ff7c1e306656c029eac0a3a46c8
geraltguru <geraltguru@gmail.com>
2024-06-10 08:53:29 +0000

    crud

---
+++ lib/models/note_database.dart
@@ -0,0 +1,43 @@
+import 'package:isar/isar.dart';
+import 'package:myapp/models/notes.dart';
+import 'package:path_provider/path_provider.dart';
+
+class NoteDatabase {
+  static late Isar isar;
+
+  static Future<void> initialize() async {
+    final dir = await getApplicationDocumentsDirectory();
+
+    isar = await Isar.open([NoteSchema], directory: dir.path);
+  }
+
+  final List<Note> currentNotes = [];
+
+  Future<void> addNote(String text) async {
+    final newNote = Note()..text = text;
+
+    isar.writeTxn(() => isar.notes.put(newNote));
+
+    fetchNotes();
+  }
+
+  Future<void> fetchNotes() async {
+    List<Note> fetchedNotes = await isar.notes.where().findAll();
+    currentNotes.clear();
+    currentNotes.addAll(fetchedNotes);
+  }
+
+  Future<void> updateNote(int id, String newText)async{
+    final existingNote = await isar.notes.get(id);
+    if(existingNote != null){
+      existingNote.text = newText;
+      await isar.writeTxn(() => isar.notes.put(existingNote));
+      await fetchNotes();
+    }
+  }
+
+  Future<void> deleteNote(int id)async{
+    await isar.writeTxn(() => isar.notes.delete(id));
+    await fetchNotes();
+  }
+}
